99fd41e9a11e3a3188d0f5ed8e0ab88391d3d397
ankit-github123 <ankit190900@gmail.com>
2020-08-24 22:38:26 +0530

    second commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react"
+import React,{useState} from "react"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./News.css"
@@ -20,19 +20,13 @@ const News = () => {
         setsearchnews(response.data.articles);
 
     }
-    const [topheadlines, settopheadlines] = useState([]);
-    useEffect(() => {
-        axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=593844fddaf24b28a25ac0026eb70125").then((res) => {
-          settopheadlines(res.data.articles);
-          console.log(res.data.articles);
-        });
-      }, []);
+  
     return (
         <div>
             <SearchNews onsubmit={onTermSubmit} searchnews={searchnews}/>
             <div className="container">
             {searchnews===null?null:<NewsSlider topheadlines={searchnews}/>}
-                <NewsSlider topheadlines={topheadlines} />
+               
 
             </div>
         </div>
